1cc2ad286e2f7dfa15d54bd3f64f1d4f61f41c88
Luiz <luizcb1944@hotmail.com>
2022-07-13 11:18:25 -0300

    Consulta com varios criterios

--- src/main/java/com/luizbati/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/luizbati/workshopmongo/repository/PostRepository.java
@@ -1,5 +1,5 @@
 package com.luizbati.workshopmongo.repository;
-
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -9,12 +9,13 @@ import org.springframework.stereotype.Repository;
 import com.luizbati.workshopmongo.domain.Post;
 
 @Repository
-public interface PostRepository extends MongoRepository<Post, String>{
+public interface PostRepository extends MongoRepository<Post, String> {
 
-	List<Post>findByTitleContainingIgnoreCase(String text);
-	
-	@Query("{'title':{<field>:{$regex: ?0, $options: '<i>}}")
-	List<Post>findByTitle(String text);
-	
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
 
+	List<Post> findByTitleContainingIgnoreCase(String text);
+	
+	@Query("{ $and: [ { date: {$gte: ?1} }, { date: { $lte: ?2} } , { $or: [ { 'title': { $regex: ?0, $options: 'i' } }, { 'body': { $regex: ?0, $options: 'i' } }, { 'comments.text': { $regex: ?0, $options: 'i' } } ] } ] }")
+	List<Post> fullSearch(String text, Date minDate, Date maxDate);
 }
